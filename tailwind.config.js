1523d26ed8e9cdb8bfeb0d9193bb89917b5143c4
Rortan <samaritanojr006@gmail.com>
2022-03-16 21:05:23 +0100

    fix/feat: removed ghost img drag & tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,31 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     mode: "jit",
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     theme: {
         extend: {
+            colors: {
+                contrast: "#10A37F",
+                light: "#dffff7",
+            },
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
             },
+            animation: {
+                vanish: "vanish 1s ease-out normal forwards",
+            },
+            keyframes: {
+                vanish: {
+                    from: {
+                        opacity: "100%",
+                    },
+                    to: {
+                        transform: "translate(100px, -999px)",
+                        zIndex: "30",
+                        opacity: "0%",
+                    },
+                },
+            },
         },
-    },
-    variants: {
-        extend: {},
     },
 };
