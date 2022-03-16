91be563bb47745ec6332d63b5fad0c69f7c6bec9
Rortan <samaritanojr006@gmail.com>
2022-03-16 20:55:53 +0100

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     env: {
         node: true,
         es6: true,
+        browser: true,
     },
     parserOptions: { ecmaVersion: 8, sourceType: "module" },
     ignorePatterns: ["node_modules/*"],
@@ -42,8 +43,9 @@ module.exports = {
                         patterns: ["@/features/*/*"],
                     },
                 ],
-                "linebreak-style": ["error", "unix"],
+                "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"],
                 "react/prop-types": "off",
+                "no-unused-vars": ["off", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
 
                 "import/order": [
                     "warn",
@@ -53,20 +55,20 @@ module.exports = {
                         alphabetize: { order: "asc", caseInsensitive: true },
                     },
                 ],
+
                 "import/default": "off",
                 "import/no-named-as-default-member": "off",
                 "import/no-named-as-default": "off",
 
                 "react/react-in-jsx-scope": "off",
-
                 "jsx-a11y/anchor-is-valid": "off",
 
-                "@typescript-eslint/no-unused-vars": ["error"],
-
+                "@typescript-eslint/no-unused-vars": ["warn"],
                 "@typescript-eslint/explicit-function-return-type": ["off"],
                 "@typescript-eslint/explicit-module-boundary-types": ["off"],
-                "@typescript-eslint/no-empty-function": ["off"],
+                "@typescript-eslint/no-empty-function": ["warn"],
                 "@typescript-eslint/no-explicit-any": ["off"],
+                "@typescript-eslint/no-var-requires": "warn",
 
                 "prettier/prettier": ["warn", {}, { usePrettierrc: true }],
             },
