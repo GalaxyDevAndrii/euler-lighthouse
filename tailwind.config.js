e2838d1840fc0917a07b21306aa221fd9f8bb5b2
Rortan <samaritanojr006@gmail.com>
2022-04-14 16:43:08 +0200

    tailwind pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,21 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    mode: "jit",
+    darkMode: "class",
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     theme: {
         extend: {
             colors: {
+                mainLight: "#dffff7",
                 contrast: "#10A37F",
-                light: "#dffff7",
+                contrastVariant: "#143037",
+                mainDark: "#121212",
+                lightDark: "#232325",
+                lightGray: "#5a5a5a",
+                mainGray: "#383b40",
             },
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-            },
-            animation: {
-                vanish: "vanish 1s ease-out normal forwards",
-            },
-            keyframes: {
-                vanish: {
-                    from: {
-                        opacity: "100%",
-                    },
-                    to: {
-                        transform: "translate(100px, -999px)",
-                        zIndex: "30",
-                        opacity: "0%",
-                    },
-                },
             },
         },
     },
