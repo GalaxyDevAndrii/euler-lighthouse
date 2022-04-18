9e5d8451b4f8dbc38a2ee197b393810c3cbc6368
Rortan <samaritanojr006@gmail.com>
2022-04-18 21:09:24 +0200

    feat: pan & zoom hooks

---
+++ src/hooks/usePan.tsx
@@ -0,0 +1,80 @@
+import { MouseEvent as SyntheticMouseEvent, TouchEvent as SyntheticTouchEvent, RefObject, useCallback, useRef, useState } from "react";
+
+import { Point } from "../types";
+import { isTouchEvent, getDelta } from "../utils/tracking";
+
+const ORIGIN = Object.freeze({ x: 0, y: 0 });
+
+type eventP = (e: MouseEvent | TouchEvent) => void;
+
+/**
+ * Track the user's intended panning offset by listening to `mousemove` events
+ * once the user has started panning.
+ */
+export default function usePan(ref: RefObject<HTMLElement | null>): [Point, (e: SyntheticMouseEvent | SyntheticTouchEvent) => void] {
+    const [panState, setPanState] = useState<Point>(ORIGIN);
+
+    const lastPointRef = useRef(ORIGIN); // last observed mouse position on pan.
+
+    const pan = useCallback((e: SyntheticMouseEvent | SyntheticTouchEvent) => {
+        e.preventDefault();
+        if (isTouchEvent(e) && e.touches.length > 1) return;
+
+        const lastPoint = lastPointRef.current;
+        const point = isTouchEvent(e) ? { x: e.touches[0].pageX, y: e.touches[0].pageY } : { x: e.pageX, y: e.pageY };
+
+        lastPointRef.current = point;
+
+        setPanState((panState) => {
+            const delta = getDelta(lastPoint, point); // delta between the last mouse position
+            const offset = getDelta(panState, delta); // apply that delta to the current pan offset
+
+            return offset;
+        });
+    }, []);
+
+    // event listeners
+    // todo: more readability
+    const endPan = useCallback(() => {
+        if (!ref.current) return;
+        ref.current.removeEventListener("mousemove", pan as unknown as eventP);
+        ref.current.removeEventListener("mouseup", endPan);
+    }, [ref, pan]);
+
+    const startSwipe = useCallback(
+        (e: TouchEvent) => {
+            if (!ref.current) return;
+            ref.current.addEventListener("touchmove", pan as unknown as eventP);
+            ref.current.addEventListener("touchend", endPan);
+            ref.current.addEventListener("touchcancel", endPan);
+
+            lastPointRef.current = { x: e.changedTouches[0].pageX, y: e.changedTouches[0].pageY };
+        },
+        [endPan, pan, ref]
+    );
+
+    const endSwipe = useCallback(() => {
+        if (!ref.current) return;
+        ref.current.removeEventListener("touchmove", pan as unknown as eventP);
+        ref.current.removeEventListener("touchend", endSwipe);
+        ref.current.removeEventListener("touchstart", startSwipe);
+    }, [pan, ref, startSwipe]);
+
+    const startPan = useCallback(
+        (e: SyntheticMouseEvent | SyntheticTouchEvent) => {
+            if (!ref.current) return;
+            ref.current.addEventListener("mousemove", pan as unknown as eventP);
+            ref.current.addEventListener("mouseup", endPan);
+            ref.current.addEventListener("touchstart", startSwipe);
+
+            if (isTouchEvent(e)) {
+                lastPointRef.current = { x: e.touches[0].pageX, y: e.touches[0].pageY };
+            } else {
+                lastPointRef.current = { x: e.pageX, y: e.pageY };
+            }
+        },
+        [endPan, pan, ref, startSwipe]
+    );
+
+    return [panState, startPan];
+}
---
+++ src/hooks/useZoom.tsx
@@ -0,0 +1,90 @@
+import { RefObject, useState, useCallback, TouchEvent as SyntheticTouchEvent } from "react";
+
+import { between } from "../utils/math";
+import { isTouchEvent, getPointFromTouch, getDistanceBetweenPoints } from "../utils/tracking";
+
+import useEventListener from "./useEventListener";
+
+type ScaleOpts = {
+    direction: "up" | "down";
+    interval: number;
+};
+
+const MIN_SCALE = 0.5;
+const MAX_SCALE = 3;
+
+/**
+ * scroll & touch zoom.
+ */
+export default function useZoom(ref: RefObject<HTMLElement | null>) {
+    const [scale, setScale] = useState(1);
+    const [lastDistance, setLastDistance] = useState(0);
+
+    const updateScale = useCallback(({ direction, interval }: ScaleOpts) => {
+        setScale((currentScale) => {
+            let scale: number;
+
+            // Adjust up to or down to the maximum or minimum scale levels by `interval`.
+            if (direction === "up" && currentScale + interval < MAX_SCALE) {
+                scale = currentScale + interval;
+            } else if (direction === "up") {
+                scale = MAX_SCALE;
+            } else if (direction === "down" && currentScale - interval > MIN_SCALE) {
+                scale = currentScale - interval;
+            } else if (direction === "down") {
+                scale = MIN_SCALE;
+            } else {
+                scale = currentScale;
+            }
+
+            return scale;
+        });
+    }, []);
+
+    const handlePinchStart = useCallback(
+        (e: SyntheticTouchEvent) => {
+            const pointA = getPointFromTouch(e.touches[0], ref);
+            const pointB = getPointFromTouch(e.touches[1], ref);
+
+            if (pointA && pointB) {
+                setLastDistance(getDistanceBetweenPoints(pointA, pointB));
+            }
+        },
+        [ref]
+    );
+
+    const handlePinchMove = useCallback(
+        (e: SyntheticTouchEvent) => {
+            if (isTouchEvent(e) && e.touches.length == 2) {
+                const pointA = getPointFromTouch(e.touches[0], ref);
+                const pointB = getPointFromTouch(e.touches[1], ref);
+
+                if (pointA && pointB) {
+                    const distance = getDistanceBetweenPoints(pointA, pointB);
+
+                    setScale((currentScale) => between(MIN_SCALE, MAX_SCALE, currentScale * (distance / lastDistance)));
+                    setLastDistance(distance);
+                }
+            }
+        },
+        [lastDistance, ref]
+    );
+
+    const handleEvent = useCallback(
+        (e: WheelEvent) => {
+            e.preventDefault();
+
+            updateScale({
+                direction: between(-1, 1, e.deltaY) > 0 ? "down" : "up",
+                interval: 0.1,
+            });
+        },
+        [updateScale]
+    );
+
+    useEventListener(ref, "wheel", handleEvent);
+    useEventListener(ref, "touchstart", handlePinchStart as any, { passive: true });
+    useEventListener(ref, "touchmove", handlePinchMove as any, { passive: true });
+
+    return scale;
+}
