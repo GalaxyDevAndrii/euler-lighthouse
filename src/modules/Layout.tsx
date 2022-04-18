0f1b1963bf0d3e84c8c83a6b774579c49cb277f7
Rortan <samaritanojr006@gmail.com>
2022-04-18 21:02:06 +0200

    feat: layout component

---
+++ src/modules/Layout.tsx
@@ -0,0 +1,19 @@
+import { useStore as useUtilsStore } from "../store/utils";
+
+import Display from "./Display";
+import Sidebar from "./Sidebar";
+import Toolbar from "./Toolbar";
+
+export default function Layout() {
+    const sidebarExpanded = useUtilsStore((state) => state.sidebarExpanded);
+
+    return (
+        <div className="h-full w-full overflow-hidden">
+            <Toolbar />
+            <Sidebar />
+            <div className={`${sidebarExpanded ? "pl-64" : "pl-0"} transition-[padding] pt-12 w-full h-full fixed`}>
+                <Display />
+            </div>
+        </div>
+    );
+}
