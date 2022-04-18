c2740883062b19e07bbd98989e68a6aea46db2a6
Rortan <samaritanojr006@gmail.com>
2022-04-18 20:29:15 +0200

    style: tailwind anim

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     theme: {
         extend: {
+            screens: {
+                xs: "395px",
+                mini: "362px",
+            },
             colors: {
                 mainLight: "#dffff7",
                 contrast: "#10A37F",
-                contrastVariant: "#143037",
                 mainDark: "#121212",
                 lightDark: "#232325",
                 lightGray: "#5a5a5a",
@@ -16,6 +19,15 @@ module.exports = {
             },
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+            animation: {
+                wiggle: "wiggle .5s ease infinite",
+            },
+            keyframes: {
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-3deg)" },
+                    "50%": { transform: "rotate(3deg)" },
+                },
             },
         },
     },
