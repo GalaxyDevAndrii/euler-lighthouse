65cc9ce5e58daadaae8d4119d335a55370ddc3bc
Rortan <samaritanojr006@gmail.com>
2022-04-18 21:07:23 +0200

    refactor/feat: node types, anim & naming

--- src/features/nodeHandler.ts
+++ src/features/nodeHandler.ts
@@ -1,7 +1,10 @@
-import { useStore } from "../store/utils";
-import { CreateNode, Nodes, INode, ConnectNodes } from "../types";
+import { toast } from "react-toastify";
 
-const findId = (nodes: Nodes): number => {
+import { useStore } from "../store/nodes";
+import { useStore as useUtilsStore } from "../store/utils";
+import { CreateNode, Nodes, INode, ConnectNodes, NodeTypes, NodeTypesArr } from "../types/index.d";
+
+const generateId = (nodes: Nodes): number => {
     const arr = nodes.map((node) => node.id);
     const n = arr.length;
     let total = ((n + 2) * (n + 1)) / 2;
@@ -20,14 +23,16 @@ class Node implements INode {
     public isActive: boolean;
     public connectedTo: number[];
     public domEl: HTMLElement | undefined;
+    public type: NodeTypes;
 
     constructor(nodes: Nodes) {
-        this.id = findId(nodes);
-        this.posX = Math.abs(useStore.getState().gridMiddle.x);
-        this.posY = Math.abs(useStore.getState().gridMiddle.y);
+        this.id = generateId(nodes);
+        this.posX = Math.abs(useUtilsStore.getState().gridMiddle.x);
+        this.posY = Math.abs(useUtilsStore.getState().gridMiddle.y);
         this.isActive = false;
         this.connectedTo = [];
         this.domEl = undefined;
+        this.type = "Normal";
     }
 
     public updatePos(pos: number[]) {
@@ -43,13 +48,26 @@ class Node implements INode {
         this.connectedTo = [...this.connectedTo, to.id];
     }
 
-    public removeConnection(node: INode) {
-        const newConnections = this.connectedTo.filter((i) => i !== node.id);
+    public removeConnection(nodeId: number) {
+        const newConnections = this.connectedTo.filter((i) => i !== nodeId);
         this.connectedTo = newConnections;
     }
 
     public clearConnections() {
         this.connectedTo = [];
+    }
+
+    public changeType() {
+        const nextType = NodeTypesArr[(NodeTypesArr.indexOf(this.type) + 1) % NodeTypesArr.length];
+        this.type = nextType;
+
+        return nextType;
+    }
+
+    public animate(animation: Keyframe[] | PropertyIndexedKeyframes, options: number | KeyframeAnimationOptions) {
+        if (this.domEl) {
+            this.domEl.animate(animation, options);
+        }
     }
 }
 
@@ -64,4 +82,21 @@ export const connectNodes: ConnectNodes = (node1, node2) => {
 
     node2.addConnection(node1);
     node1.addConnection(node2);
+};
+
+export const handleRemove = (selectedNode?: INode) => {
+    if (selectedNode) {
+        useStore.getState().removeNode(selectedNode.id);
+        useStore.getState().selectNode(undefined);
+    } else {
+        toast.error("Select a node to be removed.", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
 };
