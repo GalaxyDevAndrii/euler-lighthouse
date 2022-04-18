162b6f2dfe3298217cb5379e80c2dc73efd3ec31
Rortan <samaritanojr006@gmail.com>
2022-04-18 21:05:48 +0200

    perf: use react-tracked

--- src/components/GhostNode.tsx
+++ src/components/GhostNode.tsx
@@ -1,14 +1,14 @@
 import { useCursorPosition } from "../hooks/useCursorPosition";
 import { useStore } from "../store/nodes";
-import { useStore as useUtilsStore } from "../store/utils";
+import { useTrackedStore } from "../store/utils";
 
 import Edge from "./Edge";
 
 export default function GhostNode({ existingPairings }: { existingPairings: Set<string> }) {
-    const cursorPos = useCursorPosition();
-
     const selectedNode = useStore((state) => state.selectedNode);
-    const lineActive = useUtilsStore((state) => state.lineActive);
+    const { lineActive, sidebarExpanded } = useTrackedStore();
+
+    const cursorPos = useCursorPosition(null, { x: sidebarExpanded ? 256 : 0, y: 48 });
 
     return lineActive && selectedNode?.domEl ? (
         <>
@@ -16,8 +16,8 @@ export default function GhostNode({ existingPairings }: { existingPairings: Set<
                 id="ghost__node"
                 className="absolute w-5 h-5 left-0 top-0 will-change-auto"
                 style={{
-                    top: `${cursorPos.y - 55}px`,
-                    left: `${cursorPos.x - 265}px`,
+                    left: `${cursorPos.x}px`,
+                    top: `${cursorPos.y}px`,
                 }}
             />
             <Edge from="ghost__node" to={selectedNode.domEl.id.toString()} existingPairings={existingPairings} />
