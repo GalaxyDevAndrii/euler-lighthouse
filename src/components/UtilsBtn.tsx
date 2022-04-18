2430edac7be886207356ea62d84c3b34dd757114
Rortan <samaritanojr006@gmail.com>
2022-04-18 20:56:27 +0200

    refactor: react children type & btn style

--- src/components/UtilsBtn.tsx
+++ src/components/UtilsBtn.tsx
@@ -1,7 +1,7 @@
-import { ReactChild, ReactChildren } from "react";
+import { ReactNode } from "react";
 
 interface Props {
-    children: ReactChild | ReactChild[] | ReactChildren;
+    children: ReactNode | ReactNode[];
     classNames?: string;
     draggable?: boolean;
     id?: string;
@@ -15,7 +15,7 @@ export default function UtilsBtn({ children, classNames, ...props }: Props) {
     return (
         <button
             {...props}
-            className={`w-full bg-gray-200 rounded-md focus:border-contrast focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-contrast focus-visible:ring-offset-2 focus-visible:border-contrast active:bg-gray-300 ${classNames}`}
+            className={`w-full bg-gray-200 dark:bg-lightDark dark:border-lightGray dark:border dark:text-white rounded-md focus:border-contrast focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-contrast focus-visible:ring-offset-2 focus-visible:border-contrast active:bg-gray-300 ${classNames}`}
         >
             {children}
         </button>
