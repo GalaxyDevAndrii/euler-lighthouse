04a136cd182920727918a0a065dcecdfc0a32817
Rortan <samaritanojr006@gmail.com>
2022-04-29 23:13:52 +0200

    Change tailwind pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,14 @@ module.exports = {
                 mini: "362px",
             },
             colors: {
-                mainLight: "#dffff7",
                 contrast: "#10A37F",
+                semiLight: "#C9FBE8",
+                mainLight: "#5EDBB4",
+                spot: "#8AA1FF",
                 mainDark: "#121212",
+                mainGray: "#383b40",
                 lightDark: "#232325",
                 lightGray: "#5a5a5a",
-                mainGray: "#383b40",
             },
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
